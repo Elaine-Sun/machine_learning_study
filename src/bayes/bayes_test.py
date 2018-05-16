c96066202cc796ca84ca6bbbf111a71e9a88486c
Yiling Sun <yiling.sun@dianrong.com>
2018-05-16 17:26:44 +0800

    update

---
+++ src/bayes/bayes_test.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def load_data_set():
+    posting_list=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    class_vec = [0,1,0,1,0,1]
+    return posting_list,class_vec
+
+# def create_vocal_list(dataset)
+
